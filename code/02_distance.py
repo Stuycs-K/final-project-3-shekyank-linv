3ebc9e94640eb604ae5fbf5c59d780f042b4235e
Vi <linvincent969@gmail.com>
2023-05-19 16:15:53 -0400

    Create working version of distance optimization

--- code/02_distance.py
+++ code/02_distance.py
@@ -23,9 +23,9 @@ f2 = int(average / f1)
 # From p1 to p2
 def getShift(p1, p2):
     if(p2 < p1):
-        return (p1-p2) * "-"
+        return (p1-p2) * "+"
     else:
-        return (p2-p1) * "+"
+        return (p2-p1) * "-"
     
 def getMove(n1, n2):
     if(n2 < n1):
@@ -33,9 +33,9 @@ def getMove(n1, n2):
     else:
         return (n2-n1) * ">"
 
-registers = [average]
-bestregs = [0]
-instList = []
+registers = [average] # Set starting register
+bestregs = [0] # Set starting registry
+instList = [] # Create instruction list
 for i in message:
     bestreg = 0
     inst = getShift(ord(i), registers[bestreg])
@@ -47,29 +47,17 @@ for i in message:
         registers.append(average)
         bestreg = len(registers)-1
         inst = getShift(ord(i), registers[bestreg])
-    
     registers[bestreg] = ord(i)
     inst = getMove(bestregs[len(bestregs)-1], bestreg) + inst
     bestregs.append(bestreg)
     instList.append(inst)
 
-# print(instList)
-# print(bestregs)
+instList = '.'.join(instList)+'.'
 
-instList = '.'.join(instList)
-print(instList)
-
-#write to file
-# file = open(sys.argv[2], "w")
-# file.write(">" + "+" * len(message)) #setup
-# file.write("[[>]>" + "+" * f2)
-# file.write("[<" + "+" * f1 + ">-]<[<]>-]>") #set registers to f1*f2
-# for i in range(len(distances)):
-# 	if (distances[i] < 0):
-# 		file.write("-" * abs(distances[i]))
-# 	else:
-# 		file.write("+" * distances[i])
-# 	if (i < len(distances) - 1):
-# 		file.write(">")
-# file.write("[<]>.[>.]")
-# file.close()
+# write to file
+file = open(sys.argv[2], "w")
+file.write(">" + "+" * len(registers)) #setup, only need as many registers as calculated.
+file.write("[[>]>" + "+" * f2)
+file.write("[<" + "+" * f1 + ">-]<[<]>-]>") #set registers to f1*f2
+file.write(instList) #write calculated instructions
+file.close()
